f388ed9633b73fa5bfaeca99c2079a9b1f40282f
Tyler Bischoff <tbischoff2@ymail.com>
2025-05-06 16:50:22 -0400

    Created showcase app and added it's models

---
+++ showcase/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Build(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    creator = models.CharField(max_length=100)
+    tags = models.ManyToManyField('Tag', related_name='builds')
+    thumbnail = models.ForeignKey('Image', related_name='build', on_delete=models.PROTECT, null=False, blank=False)
+
+
+class Image(models.Model):
+    buildID = models.ForeignKey(Build, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='images/')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)

7d0af178c83679d4f4ec82050739bc306f0127f1
Tyler Bischoff <tbischoff2@ymail.com>
2025-05-15 16:55:41 -0400

    Added pagination to the results

--- Minecraft-Cult-Website/showcase/views.py
+++ Minecraft-Cult-Website/showcase/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 from .models import Build, Tag
 from .forms import BuildForm
 
 # Create your views here.
 def showcase(request):
-    builds = Build.objects.all()
+    builds_list = Build.objects.all()
+    paginator = Paginator(builds_list, 12)
+    page_number = request.GET.get('page')
+    builds = paginator.get_page(page_number)
+    
     tags = Tag.objects.all()
     context = {'tags': tags, 'builds': builds}
     return render(request, 'showcase.html', context)

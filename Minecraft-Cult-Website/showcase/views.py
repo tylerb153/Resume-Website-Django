b28d1a1a4dfb76c04a71f11ef9ea1c9412e11919
Tyler Bischoff <tbischoff2@ymail.com>
2025-05-15 20:14:53 -0400

    Single Filter at a time with buttons (filters do nothing)

--- Minecraft-Cult-Website/showcase/views.py
+++ Minecraft-Cult-Website/showcase/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import Build, Tag
 from .forms import BuildForm
 
 # Create your views here.
 def showcase(request):
-    builds_list = Build.objects.all()
+    searchQuery = request.GET.get('search', '')
+    builds_list = Build.objects.filter(Q(title__icontains=searchQuery) | Q(creator__icontains=searchQuery) | Q(tags__name__icontains=searchQuery)).distinct()
+
+    filters = request.GET.get('filters', '')
+    
+
     paginator = Paginator(builds_list, 12)
     page_number = request.GET.get('page')
     builds = paginator.get_page(page_number)
     
     tags = Tag.objects.all()
-    context = {'tags': tags, 'builds': builds}
+    context = {'tags': tags, 'builds': builds, 'searchQuery': searchQuery}
     return render(request, 'showcase.html', context)
 
 def createBuild(request):

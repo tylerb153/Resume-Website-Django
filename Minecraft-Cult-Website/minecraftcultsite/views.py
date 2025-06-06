39fcb980a736208bc124a540d1dadd457d9be7e5
Tyler Bischoff <tbischoff2@ymail.com>
2025-06-06 18:56:32 -0400

    set timeout on requests and cleaned up the code a little

--- Minecraft-Cult-Website/minecraftcultsite/views.py
+++ Minecraft-Cult-Website/minecraftcultsite/views.py
@@ -7,17 +7,15 @@ def base(request):
 
 def mapPage(request):
     try:
-        if requests.head('https://map.tymler.com/').status_code == 200:
+        if requests.head('https://map.tymler.com/', timeout=1).status_code == 200:
             map_link = 'https://map.tymler.com/'
-        elif requests.head('https://backupmap.tymler.com/').status_code == 200:
+        elif requests.head('https://backupmap.tymler.com/', timeout=1).status_code == 200:
             map_link = 'https://backupmap.tymler.com/'
         else:
             map_link = "Offline"
     except:
         map_link = "Offline"
     
-    queryString = request.META.get('QUERY_STRING', '')
-    print(f'URL String:\n{queryString}')
-    
-    # map_link = "Offline"
+    queryString = request.META.get('QUERY_STRING', '')    
+
     return render(request, 'map.html', context={"map_link": map_link, "queryString":queryString})

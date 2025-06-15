a3a9e9c9780ae84c632bd19dc4864a31675aa4d4
Tyler Bischoff <tbischoff2@ymail.com>
2025-06-15 15:43:29 -0400

    /home redirects to /

--- Minecraft-Cult-Website/minecraftcultsite/urls.py
+++ Minecraft-Cult-Website/minecraftcultsite/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+from django.views.generic import RedirectView
+
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('home/', RedirectView.as_view(url='/', permanent=True)),
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('map/', views.mapPage, name='map'),

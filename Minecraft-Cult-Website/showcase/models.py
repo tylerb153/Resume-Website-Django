421b916368f2a6778aa898a0e5243df5f384133e
Tyler Bischoff <tbischoff2@ymail.com>
2025-06-20 19:39:06 -0400

    Deleting builds or images from the admin page deletes the files properly now

--- Minecraft-Cult-Website/showcase/models.py
+++ Minecraft-Cult-Website/showcase/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 import os
 import shutil
 
@@ -40,12 +42,6 @@ class Build(models.Model):
             counter += 1
         
         return uniqueSlug
-
-    def delete(self, *args, **kwargs):
-        imageFolder = os.path.join(settings.MEDIA_ROOT, 'builds', str(self.id))
-        super().delete(*args, **kwargs)
-        if os.path.isdir(imageFolder):
-            shutil.rmtree(imageFolder)
             
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -55,8 +51,14 @@ class Build(models.Model):
 
     def __str__(self):
         return self.title
+    
+@receiver(post_delete, sender=Build)
+def deleteBuildFiles(sender, instance, **kwargs):
+    imageFolder = os.path.join(settings.MEDIA_ROOT, 'builds', str(instance.id))
+    if os.path.isdir(imageFolder):
+        shutil.rmtree(imageFolder)
 
-def build_image_upload_path(instance, filename):
+def getImageUploadPath(instance, filename):
     if instance.build:
         return f'builds/{instance.build.id}/{filename}'
     else:
@@ -65,11 +67,16 @@ def build_image_upload_path(instance, filename):
 class Image(models.Model):
     name = models.CharField(null=True, blank=True)
     build = models.ForeignKey(Build, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to=build_image_upload_path)
+    image = models.ImageField(upload_to=getImageUploadPath)
     thumbnail = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+    
+@receiver(post_delete, sender=Image)
+def deleteImageFile(sender, instance, **kwargs):
+    if instance.image and os.path.isfile(instance.image.path):
+        os.remove(instance.image.path)
 
 class BuildTag(models.Model):
     build = models.ForeignKey(Build, on_delete=models.CASCADE)
